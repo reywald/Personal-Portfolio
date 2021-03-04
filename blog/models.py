b83a632a52bede33b9abc4b58eb12564e299dab5
Agbarakwe Ikechukwu <iaweepingsaint@gmail.com>
2021-03-04 09:06:10 +0100

    Fixed and polished blog detail page and others

--- blog/models.py
+++ blog/models.py
@@ -3,4 +3,7 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    date = models.DateField()
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
