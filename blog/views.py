2b9bdf9be788b5e2c11444b1d1c6f25fb01d0341
Agbarakwe Ikechukwu <iaweepingsaint@gmail.com>
2021-03-04 09:51:20 +0100

    Used and extended base templates, dynamic titles

--- blog/views.py
+++ blog/views.py
@@ -3,9 +3,9 @@ from .models import Blog
 
 def all_blogs(request):
     blogs = Blog.objects.order_by('-date')[:]
-    return render(request, 'blog/all_blogs.html', {'blogs': blogs})
+    return render(request, 'blog/all_blogs.html', {'blogs': blogs, 'title': 'All blogs'})
 
 
 def detail(request, blog_id):
     blog = get_object_or_404(Blog, pk=blog_id)
-    return render(request, 'blog/detail.html', {'blog': blog})
+    return render(request, 'blog/detail.html', {'blog': blog, 'title': 'Blog Details'})
